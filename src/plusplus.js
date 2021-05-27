d86c7a1920ddd210af70b713bacc88f282f4e916
Peter Nguyen <77014724+pnguyen-auth0@users.noreply.github.com>
2021-05-27 08:52:20 -0500

    Update src/plusplus.js
    Co-authored-by: Matt O'Keefe <Matt@OKeefe.dev>

--- src/plusplus.js
+++ src/plusplus.js
@@ -274,10 +274,10 @@ module.exports = function plusPlus(robot) {
       .concat('`{name1, name2, name3}++ [<reason>]` - Increment score for all names (for a reason)\n')
       .concat('`{name1, name2, name3}-- [<reason>]` - Decrement score for all names (for a reason) \n')
       .concat('`{name1, name2, name3}-- [<reason>]` - Decrement score for all names (for a reason) \n')
-      .concat('`@qrafty score <name>` - Display the score for a name and some of the reasons\n')
-      .concat('`@qrafty top <amount>` - Display the top scoring <amount>\n')
-      .concat('`@qrafty erase <name> [<reason>]` - Remove the score for a name (for a reason) \n')
-      .concat('`@qrafty level me up` - Level up your account for some additional qraftiness \n')
+      .concat('`@${msg.robot.name} score <name>` - Display the score for a name and some of the reasons\n')
+      .concat('`@${msg.robot.name} top <amount>` - Display the top scoring <amount>\n')
+      .concat('`@${msg.robot.name} erase <name> [<reason>]` - Remove the score for a name (for a reason) \n')
+      .concat('`@${msg.robot.name} level me up` - Level up your account for some additional qraftiness \n')
       .concat('`how much are <point_type> points worth` - Shows how much <point_type> points are worth\n');
 
     const message = {
