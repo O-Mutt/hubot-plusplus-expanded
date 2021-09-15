26dd7e6211a2982353d9502d580939497333539c
Matthew O'Keefe <matt@okeefe.dev>
2021-09-15 06:58:04 -0500

    Couple fixes for mapper

--- src/services/mapper.js
+++ src/services/mapper.js
@@ -11,21 +11,59 @@ async function mapUsersToDb(msg, props) {
   const web = new WebClient(msg.robot.adapter.options.token);
   const { members } = await web.users.list();
 
+  const mappings = [];
   for (const member of members) {
     try {
       msg.robot.logger.debug('Map this member', JSON.stringify(member));
       const localMember = await databaseService.getUser(member.name);
       localMember.slackId = member.id;
-      // eslint-disable-next-line no-underscore-dangle
       if (localMember._id) {
         await db.collection(scoresDocumentName).replaceOne({ name: localMember.name }, localMember);
+        mappings.push(`{ name: ${localMember.name}, slackId: ${localMember.slackId}, id: ${localMember._id} }`);
       }
       msg.robot.logger.debug(`Save the new member ${JSON.stringify(localMember)}`);
     } catch (er) {
       msg.robot.logger.error('failed to find', member, er);
     }
   }
-  msg.reply(`Ding fries are done. We mapped ${members.length} users`);
+  msg.reply(`Ding fries are done. We mapped ${mappings.length} of ${members.length} users. \n${mappings.join('\n')}`);
+}
+
+async function mapSingleUserToDb(msg, props) {
+  const { robot } = msg;
+  const { mentions } = msg.message;
+  if (!mentions) {
+    msg.reply('You need to @ someone to map.');
+    return;
+  }
+  const userMentions = mentions.filter((men) => men.type === 'user');
+  userMentions.shift(); // shift off @hubot
+  const to = userMentions.shift();
+  const databaseService = new DatabaseService({ robot, ...props });
+  await databaseService.init();
+  const db = await databaseService.getDb();
+
+  const web = new WebClient(msg.robot.adapter.options.token);
+  const { members } = await web.users.list();
+
+  for (const member of members) {
+    try {
+      if (member.name === to.name) {
+        msg.robot.logger.debug('Map this member', JSON.stringify(member));
+        const localMember = await databaseService.getUser(member.name);
+        localMember.slackId = member.id;
+        // eslint-disable-next-line no-underscore-dangle
+        if (localMember._id) {
+          await db.collection(scoresDocumentName).replaceOne({ name: localMember.name }, localMember);
+          msg.reply(`Mapping completed for ${to.name}: { name: ${localMember.name}, slackId: ${localMember.slackId}, id: ${localMember._id} }`);
+          return;
+        }
+        msg.robot.logger.debug(`Save the new member ${JSON.stringify(localMember)}`);
+      }
+    } catch (er) {
+      msg.robot.logger.error('failed to find', member, er);
+    }
+  }
 }
 
 async function unmapUsersToDb(msg, props) {
@@ -42,4 +80,4 @@ async function unmapUsersToDb(msg, props) {
   msg.reply('Ding fries are done. We unmapped all users');
 }
 
-module.exports = { mapUsersToDb, unmapUsersToDb };
+module.exports = { mapUsersToDb, unmapUsersToDb, mapSingleUserToDb };
