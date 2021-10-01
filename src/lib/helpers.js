d768f1a4669bb0876d6969caec2ca87dbefbcc1f
Matthew O'Keefe <matt@okeefe.dev>
2021-10-01 12:47:31 -0500

    fixing parse int issue

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -195,7 +195,7 @@ function getProcessVariables(env) {
   procVars.notificationsRoom = env.HUBOT_PLUSPLUS_NOTIFICATION_ROOM || undefined;
   procVars.falsePositiveNotificationsRoom = env.HUBOT_PLUSPLUS_FALSE_POSITIVE_NOTIFICATION_ROOM || undefined;
   procVars.monthlyScoreboardCron = env.HUBOT_PLUSPLUS_MONTHLY_SCOREBOARD_CRON || '0 10 1-7 * *';
-  procVars.monthlyScoreboardDayOfWeek = env.HUBOT_PLUSPLUS_MONTHLY_SCOREBOARD_DAY_OF_WEEK || 1; // 0-6 (Sun - Sat)
+  procVars.monthlyScoreboardDayOfWeek = parseInt(env.HUBOT_PLUSPLUS_MONTHLY_SCOREBOARD_DAY_OF_WEEK, 10) || 1; // 0-6 (Sun - Sat)
   return procVars;
 }
 
